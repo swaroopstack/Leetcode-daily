dd8046aecfc79215f99c8e2cf06838395d8e519d
Swaroop Kumar <swarooppokemon@gmail.com>
2026-08-10 20:36:29 +0530

    Time: 3 ms (64.78%), Space: 8.4 MB (11.29%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(int n,vector<int> &dp){
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    } 
+    int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        return solve(n,dp);
+    }
+};
