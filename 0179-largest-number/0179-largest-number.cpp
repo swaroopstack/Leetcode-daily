e47dc3cad549f6f5236bcc34fe7431999a6675a9
Swaroop Kumar <swarooppokemon@gmail.com>
2026-08-15 15:16:29 +0530

    Time: 3 ms (74.39%), Space: 15.1 MB (96.42%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), [](auto& a, auto& b) {
+            string x = to_string(a);
+            string y = to_string(b);
+            return x+y>y+x;
+        });
+        string ans="";
+        for (int num : nums) {
+            ans += to_string(num);
+        }
+        if (ans[0] == '0') {
+            return "0";
+        }
+        return ans;
+    }
+};
