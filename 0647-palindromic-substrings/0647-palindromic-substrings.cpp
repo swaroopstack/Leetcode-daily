2b4d0b87759344c18c2a7a5173f6c5a6c59d44e9
Swaroop Kumar <swarooppokemon@gmail.com>
2026-09-10 11:58:01 +0530

    Time: 1903 ms (5.01%), Space: 428.8 MB (6.68%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
+    int ans=0;
+    int solve(int i,int j,string s,vector<vector<int>>& dp){
+        if(i>=j) return true;
+        if(s[i]!=s[j]) return false;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        
+        return dp[i][j]=solve(i+1,j-1,s,dp);
+    }
     int countSubstrings(string s) {
-        int ans=0;
+        vector<vector<int>> dp(s.size(),vector<int>(s.size(),-1));
         for(int i=0;i<s.size();i++){
             for(int j=i;j<s.size();j++){
-                bool palindrome=true;
-                int low=i;
-                int high=j;
-                while(low<=high){
-                    if(s[low]!=s[high]){
-                        palindrome=false;
-                        break;
-                    }
-                    low++;
-                    high--;
-                }
-                if(palindrome){
+                if(solve(i,j,s,dp)){
                     ans++;
                 }
             }
