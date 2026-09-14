1a88a7d3ec7652749dac6f576efea82c65af2cee
Swaroop Kumar <swarooppokemon@gmail.com>
2026-09-14 21:36:40 +0530

    Time: 0 ms (100%), Space: 9.3 MB (41.02%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char,int> mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        int ans=0;
+        bool odd=false;
+        for(auto &it : mp){
+            if(it.second%2==0){
+                ans+=it.second;
+            }
+            else {
+                ans += it.second - 1;
+                odd = true;
+            }
+        }
+        if(odd){
+            ans++;
+        }
+        return ans;
+    }
+};
